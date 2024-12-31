d96312f2eb37654b17dc603259407384929aecaf
Jacobwkidd <kiddj1@students.westerntc.edu>
2024-12-31 00:20:41 -0600

    I almost got done experience component. I just have to work on the desktop screen.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './features/contact/contact.component';
+import { ExperienceComponent } from './features/experience/experience.component';
 import { HomeComponent } from './features/home/home.component';
-import { FullstackComponent } from './features/fullstack/fullstack.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Default route
-  { path: 'fullstack', component: FullstackComponent },
+  { path: '', component: HomeComponent },
+  { path: 'experience', component: ExperienceComponent }, // New Route
+  { path: 'contact', component: ContactComponent },
 ];
