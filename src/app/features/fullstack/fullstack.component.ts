8fc9a64aaa3dd9c681aafc7fbf8834e899111a97
Jacobwkidd <kiddj1@students.westerntc.edu>
2025-01-07 20:27:34 -0600

    made some projects

--- src/app/features/fullstack/fullstack.component.ts
+++ src/app/features/fullstack/fullstack.component.ts
@@ -17,7 +17,7 @@ export class FullstackComponent {
       tags: ['Angular', 'TypeScript', 'Tailwind', 'CSS', 'JSON'],
       image: 'https://via.placeholder.com/300',
       github: 'https://github.com/your-repo',
-      detailedDescription: `This Phone Store Management Application is a responsive web platform designed for seamless phone inventory management. Built with Angular and styled using Tailwind CSS, the application features robust API communication to facilitate real-time data exchange between the frontend and a mock database. It supports full CRUD (Create, Read, Update, Delete) operations, enabling users to efficiently manage phone records. Utilizing Angular's two-way data binding with [(ngModel)], the app ensures dynamic synchronization between the user interface and the backend, providing a smooth and interactive user experience. The application also prioritizes responsive design, ensuring usability across devices, and serves as a scalable solution for future integration with production-ready databases or advanced features.`,
+      detailedDescription: `A responsive web platform built with Angular and styled using Tailwind CSS, enabling users to perform CRUD operations on phone data through seamless API communication, two-way data binding, and a mock database for efficient management.`,
     },
     {
       title: 'HR and Product Management System',
@@ -42,7 +42,16 @@ export class FullstackComponent {
       description: 'Employees can punch in and out while calculating the time with the hourly wage.',
       tags: ['p5.js', 'JavaScript', 'HTML'],
       image: 'https://via.placeholder.com/300',
-      github: 'https://github.com/Jacobwkidd',
+      github: 'https://github.com/Jacobwkidd/Heavy-Weapons-demo',
+      detailedDescription:
+      'This project includes features for managing employee punch-ins and outs, hourly wage calculation, and seamless integration with payroll systems.',
+    },
+    {
+      title: 'Web Programming with Dotnet',
+      description: 'Employees can punch in and out while calculating the time with the hourly wage.',
+      tags: ['Dotnet', 'C#', 'SQL'],
+      image: 'https://via.placeholder.com/300',
+      github: 'https://github.com/Jacobwkidd/WPwithDotnet/tree/main/UserLoginApp',
       detailedDescription:
       'This project includes features for managing employee punch-ins and outs, hourly wage calculation, and seamless integration with payroll systems.',
     },
