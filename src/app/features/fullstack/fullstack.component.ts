dc4b846f96f7f659a1fd2306250f30fd58a099be
Jacobwkidd <kiddj1@students.westerntc.edu>
2025-04-24 19:01:15 -0500

    made a lot of changes

--- src/app/features/fullstack/fullstack.component.ts
+++ src/app/features/fullstack/fullstack.component.ts
@@ -23,14 +23,14 @@ export class FullstackComponent {
     {
       title: 'Stock Market',
       tags: ['Angular', 'Nestjs', 'SQL', 'PostgreSQL'],
-      image: 'https://via.placeholder.com/300',
-      github: 'https://github.com/Jacobwkidd/WPwithDotnet/tree/main/UserLoginApp',
+      image: '../../../assets/Projects/StockPulse.png',
+      github: 'https://github.com/Jacobwkidd/Stock-Market-App',
       detailedDescription:
       'This project includes features for managing employee punch-ins and outs, hourly wage calculation, and seamless integration with payroll systems.',
     },
     {
       title: 'HR and Product Management System',
-      tags: ['React', 'NestJS', 'Tailwind', 'MySQL', 'PhpMyAdmin'],
+      tags: ['React', 'NestJS', 'Tailwind', 'MySQL'],
       image: 'https://via.placeholder.com/300',
       github: 'https://github.com/Jacobwkidd/inventory-system.git',
       detailedDescription:
@@ -63,7 +63,7 @@ export class FullstackComponent {
     {
       title: 'This Website',
       tags: ['Angular', 'TypeScript', 'Tailwind', 'CSS'],
-      image: '../../../assests/MWP.png',
+      image: '../../../assets/Projects/MyWebsite.png',
       github: 'https://github.com/Jacobwkidd/my-website-app.git',
       detailedDescription: `I like this interactive and responsive design showcased in this modern portfolio platform, which highlights a collection of professional full-stack development projects. Built with Angular and styled using Tailwind CSS, the website emphasizes clean design, dynamic content, and user-friendly interfaces that work seamlessly across devices. Serving as a central hub for sharing expertise in web and software development, it demonstrates technical proficiency and creative problem-solving skills. With a future-ready architecture, the platform is designed for scalability, allowing effortless addition of new projects and features. Its robust technology stack, including Angular for dynamic interfaces, Tailwind CSS for sleek styling, and TypeScript for type safety, ensures a cutting-edge and maintainable foundation for showcasing development work.`,
     },
