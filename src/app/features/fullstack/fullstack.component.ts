a81d17d8f2d8f21bd7f553730c88c8211936977b
Jacobwkidd <kiddj1@students.westerntc.edu>
2025-04-29 19:47:42 -0500

    Made some changes

--- src/app/features/fullstack/fullstack.component.ts
+++ src/app/features/fullstack/fullstack.component.ts
@@ -25,7 +25,7 @@ export class FullstackComponent {
     {
       title: 'Inventory Management System',
       tags: ['React', 'NestJS', 'Tailwind', 'MySQL'],
-      image: 'https://via.placeholder.com/300',
+      image: '../../../assets/Projects/Inventory System.png',
       github: 'https://github.com/Jacobwkidd/inventory-system.git',
       detailedDescription:
       'This project includes features for managing employee punch-ins and outs, hourly wage calculation, and seamless integration with payroll systems.',
@@ -39,27 +39,25 @@ export class FullstackComponent {
     },
     {
       title: 'Rental Property Management System',
-      tags: ['Tailwind', 'Vue', 'JavaScript', 'HTML', 'CSS'],
-      image: 'https://via.placeholder.com/300',
-      github: 'https://github.com/Jacobwkidd/Heavy-Weapons-demo',
-      detailedDescription:
-      'This project includes features for managing employee punch-ins and outs, hourly wage calculation, and seamless integration with payroll systems.',
+      tags: ['Vue', 'JavaScript', 'Tailwind', 'HTML', 'CSS'],
+      image: '../../../assets/Projects/Rental.png',
+      github: 'https://github.com/Jacobwkidd/vue-final-project.git',
+      detailedDescription: 'The Rental Property Management System is a web application developed using Vue, JavaScript, Tailwind CSS, HTML, and CSS. This platform allows users to search for apartments or houses by location, filter listings based on price and property type (such as duplexes, single-family homes, and more), and easily find rental opportunities that meet their needs. Landlords have the ability to post rental properties or list homes for sale directly through the system. Additionally, tenants can securely pay rent online if their landlord opts to use the platform’s integrated payment system. This project showcases my ability to build responsive, user-friendly applications with advanced filtering, search functionality, and real-world e-commerce capabilities.',
     },
     {
-      title: 'WordGame',
-      tags: ['C#', 'TypeScript', 'Tailwind', 'CSS', 'JSON'],
-      image: 'https://via.placeholder.com/300',
-      github: 'https://github.com/Jacobwkidd/WordGame',
-      detailedDescription:
-      'This project includes features for managing employee punch-ins and outs, hourly wage calculation, and seamless integration with payroll systems.',
-    },
+      "title": "WordGame",
+      "tags": ["C#", ".NET", "TypeScript", "Tailwind", "CSS", "JSON"],
+      "image": "https://via.placeholder.com/300",
+      "github": "https://github.com/Jacobwkidd/WordGame",
+      "detailedDescription": "WordGame is an interactive, real-time word guessing application built with a .NET backend and a TypeScript frontend. Players input letter guesses, which are transmitted to the server via JSON. The .NET server validates each guess by checking it against the target word and responds with immediate feedback indicating if the guess is correct. This real-time data exchange creates a smooth and engaging user experience. The frontend is styled with Tailwind CSS to ensure a modern, responsive design. This project showcases real-time server communication, dynamic client updates, efficient state management, and full-stack development with C#, .NET, and modern web technologies."
+    },    
     {
       title: 'Web Programming with Dotnet',
-      tags: ['Dotnet', 'C#', 'SQL'],
-      image: 'https://via.placeholder.com/300',
-      github: 'https://github.com/Jacobwkidd/WPwithDotnet/tree/main/UserLoginApp',
+      tags: ['Dotnet', 'Angular', 'TypeScript', 'Tailwind', 'SQL'],
+      image: '../../../assets/Projects/movies.png',
+      github: 'https://github.com/Jacobwkidd/WPwithDotnet',
       detailedDescription:
-      'This project includes features for managing employee punch-ins and outs, hourly wage calculation, and seamless integration with payroll systems.',
+      'This project is a full-stack web application built using Dotnet, Angular, and C#. It connects to the TMDB (The Movie Database) API to retrieve and display real-time data for movies and TV shows. Users can easily browse and filter content by categories such as Now Playing, Top Rated, and Popular. The backend, developed in Dotnet with C#, manages API requests and processes data efficiently, while the frontend, powered by Angular, provides a clean, responsive user interface. This project highlights my skills in API integration, dynamic content rendering, and full-stack development using modern frameworks and languages.',
     },
     {
       title: 'Phone Store',
@@ -70,7 +68,7 @@ export class FullstackComponent {
     },
     {
       title: '2d Game',
-      tags: ['p5.js', 'JavaScript', 'HTML'],
+      tags: ['JavaScript','HTML', 'CSS'],
       image: 'https://via.placeholder.com/300',
       github: 'https://github.com/Jacobwkidd/Heavy-Weapons-demo',
       detailedDescription:
